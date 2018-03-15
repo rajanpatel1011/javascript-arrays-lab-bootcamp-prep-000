5963a2259c9edfe8fae7d53647f2c9afcb63953f
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-15 16:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+var kittens = ["Milo","Otis","Garfield"]
+function destructivelyAppendKitten(name){
+kittens.push(name);
+return kittens;
+} 
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveKitten(name){
+  kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+ 
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+function appendKitten(name){
+  var oldArrary = kittens
+  var newArrary = kittens.concat(name);
+  return newArrary;
+}
+function prependKitten(name){
+  var oldArrary = kittens
+  var newArrary = [name,...kittens]
+  return newArrary;
+}
+function removeLastKitten(){
+
+    kittens.pop();
+    return kittens;
+
+}
+function removeFirstKitten(){
+  var oldArrary = kittens;
+  var newArrary = kittens.shift();
+  return newArrary;
+}
